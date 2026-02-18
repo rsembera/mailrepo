db4a4e37fca4383bcba502cfb42d6eb844be750e
rsembera <rsembera@ncf.ca>
2026-02-18 17:56:29 -0500

    Search Archive: restore Clear button, add 'Press Enter or click Search' hint

--- web/static/js/views/mail.js
+++ web/static/js/views/mail.js
@@ -235,12 +235,16 @@ function renderSearchView(results = null, query = '') {
                            placeholder="Search by subject, sender, recipient, or content..." 
                            value="${escapeHtml(query)}"
                            onkeydown="if(event.key==='Enter') executeArchiveSearch()">
-                    ${hasQuery ? '<button class="search-clear" onclick="clearArchiveSearch()" title="Clear search"><i data-lucide="x"></i></button>' : ''}
                 </div>
                 <div class="toolbar-actions">
+                    <span class="search-hint-text">Press Enter or click Search</span>
                     <button class="btn btn-primary" onclick="executeArchiveSearch()">
                         <i data-lucide="search"></i>
                         Search
+                    </button>
+                    <button class="btn btn-secondary" onclick="clearArchiveSearch()" ${!hasQuery ? 'disabled' : ''}>
+                        <i data-lucide="x"></i>
+                        Clear
                     </button>
                 </div>
             </div>
